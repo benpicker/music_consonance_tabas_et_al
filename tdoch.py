2bb4916e5d4f1560d5e16ce32b2eaf2e92409a6b
Ben Picker <benpicker@gmail.com>
2025-12-29 12:46:13 -0500

    added tdoch.py

---
+++ tdoch.py
@@ -0,0 +1,69 @@
+import numpy as np
+import scipy.io
+import os
+import string
+import random
+from loadParameters import loadParameters
+from tdochCortex import tdochCortex
+
+def tdoch(pars=None, parsing=0):
+    """
+    Python version of tdoch.m
+    Args:
+        pars: parameters dictionary or object
+        parsing: 0 (default) or string for parsing file
+    Returns:
+        s, r, lagSpace, timeSpace
+    """
+    if pars is None:
+        pars = loadParameters()
+    if parsing is None:
+        parsing = 0
+
+    r = {}
+    r['lagSpace'] = binSpace(pars)
+    r['freqSpace'] = 1000. / r['lagSpace']
+
+    if isinstance(parsing, str) and parsing:
+        timeSpace, r['A'], r['n'], r['b'] = parseThalamic(parsing)
+    else:
+        timeSpace, r['A'], r['n'], r['b'] = pyThalamic(r['lagSpace'], pars)
+
+    r['timeSpace'] = timeSpace * 1000  # convert to ms
+
+    if not getattr(pars, 'onlySubcort', False):
+        s = tdochCortex(r, pars)
+    else:
+        s = 0
+
+    return s, r, r['lagSpace'], r['timeSpace']
+
+def pyThalamic(lagSpace, pars):
+    # parse filename is randomized to allow parallel computations
+    chart = string.ascii_letters + string.digits
+    parseID = 'pyparse' + ''.join(random.choices(chart, k=4))
+    parseIn = parseID + 'In.mat'
+    parseOut = parseID + 'Out.mat'
+    scipy.io.savemat(parseIn, {'pars': pars, 'lagSpace': lagSpace})
+
+    # You may need to adjust the python command for your system
+    python_cmd = 'python'
+    os.system(f'{python_cmd} subthalamic.py {parseID}')
+
+    timeSpace, A, n, b = parseThalamic(parseOut)
+    os.remove(parseOut)
+    return timeSpace, A, n, b
+
+def parseThalamic(parsing):
+    pyparse = scipy.io.loadmat(parsing)
+    timeSpace = pyparse['timeSpace'].squeeze()
+    A = pyparse['A']
+    n = pyparse['n']
+    b = pyparse['b']
+    return timeSpace, A, n, b
+
+def binSpace(pars):
+    # Placeholder: implement binSpace logic as in MATLAB
+    # This should return the lagSpace array
+    # For now, return a dummy array
+    return np.linspace(1, 15, 15)
